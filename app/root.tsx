24e2c460f562f3dafe95c313c063cde7abca9d81
tkeiyama <thomas18thx@gmail.com>
2025-10-21 18:43:51 +0900

    add a link for favicon

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,12 @@
 import { Links } from "react-router"
+import { Route } from "./+types/root"
+
+export const links: Route.LinksFunction = () => [
+  {
+    rel: "icon",
+    href: "/favicon.ico"
+  }
+]
 
 export default function R() {
   return <html>
