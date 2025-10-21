b704d4e9cf3d4c06682251688d3b628074b73901
tkeiyama <thomas18thx@gmail.com>
2025-10-21 18:05:42 +0900

    set host to 0.0.0.0

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite'
 import { reactRouter } from '@react-router/dev/vite'
 
 export default defineConfig({
-  plugins: [reactRouter()]
+  plugins: [reactRouter()],
+  server: {
+    host: "0.0.0.0"
+  }
 })
