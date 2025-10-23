f27f977254616be158d6377495c035184ba0f8a9
tkeiyama <thomas18thx@gmail.com>
2025-10-23 22:32:43 +0900

    add css for lint and format

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,5 +2,5 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-  "*.{js,ts,tsx,json,md,yaml}": ["pnpm lint", "pnpm format"],
+  "*.{js,ts,tsx,json,md,yaml,css}": ["pnpm lint", "pnpm format"],
 };
