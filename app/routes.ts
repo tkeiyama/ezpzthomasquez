08a6ff701d910df5e82671731119f613abc9dd52
tkeiyama <thomas18thx@gmail.com>
2025-10-23 22:34:30 +0900

    register a route for /articles/:id

--- app/routes.ts
+++ app/routes.ts
@@ -1,5 +1,6 @@
-import { index, RouteConfig } from "@react-router/dev/routes";
+import { index, route, RouteConfig } from "@react-router/dev/routes";
 
 export default [
   index("./routes/index/route.tsx"),
+  route("/articles/:id", "./routes/article/route.tsx"),
 ] satisfies RouteConfig;
