26ec127049bf3d4d842519bfeca265e2523f052d
tkeiyama <thomas18thx@gmail.com>
2025-10-24 23:46:16 +0900

    set prerender

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,5 +1,12 @@
 import { Config } from "@react-router/dev/config";
+import { readdir } from "node:fs/promises";
 
 export default {
   ssr: true,
+  prerender: async ({ getStaticPaths }) => {
+    const staticPaths = getStaticPaths();
+    const articleNames = await readdir(`${process.cwd()}/articles`);
+    const dynamicPaths = articleNames.map((articleName) => `/articles/${articleName}`);
+    return [...staticPaths, ...dynamicPaths];
+  },
 } satisfies Config;
