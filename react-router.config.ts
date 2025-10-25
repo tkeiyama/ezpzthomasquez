3de73dc1caf8af9d52e6b4f7f4a6ab87074e2878
tkeiyama <thomas18thx@gmail.com>
2025-10-25 09:42:27 +0900

    set basename to /ezpzthomasquez/ in production

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,6 +2,7 @@ import { Config } from "@react-router/dev/config";
 import { readdir } from "node:fs/promises";
 
 export default {
+  basename: import.meta.env.PROD ? "/ezpzthomasquez/" : "/",
   ssr: true,
   prerender: async ({ getStaticPaths }) => {
     const staticPaths = getStaticPaths();
